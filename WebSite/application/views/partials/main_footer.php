8769705c6156b8c9c1d8e854a650f48468072c5a
Mohsinkhan88 <mohsin88@live.com>
2014-02-18 21:16:54 +0500

    Modified header and footer for Form validation controls

--- WebSite/application/views/partials/main_footer.php
+++ WebSite/application/views/partials/main_footer.php
@@ -2,9 +2,15 @@
    
      <!-- plugins -->
     <script type="text/javascript" src="<?=asset_url('plugins/charts/sparkline/jquery.sparkline.min.js');?>"></script>
+	<script type="text/javascript" src="<?=asset_url('plugins/charts/knob/jquery.knob.js');?>"></script>
     <script type="text/javascript" src="<?=asset_url('plugins/forms/uniform/jquery.uniform.min.js');?>"></script>
     <script type="text/javascript" src="<?=asset_url('plugins/forms/validate/jquery.validate.min.js');?>"></script>      
-    
+      
+	  <script type="text/javascript" src="<?=asset_url('plugins/forms/wizard/jquery.bbq.js');?>"></script>
+    <script type="text/javascript" src="<?=asset_url('plugins/forms/wizard/jquery.form.js');?>"></script>
+    <script type="text/javascript" src="<?=asset_url('plugins/forms/wizard/jquery.form.wizard.js');?>"></script>
+
+	
     <script type="text/javascript" src="<?=asset_url('plugins/forms/elastic/jquery.elastic.js');?>"></script>
     <script type="text/javascript" src="<?=asset_url('plugins/forms/inputlimiter/jquery.inputlimiter.1.3.min.js');?>"></script>
     <script type="text/javascript" src="<?=asset_url('plugins/forms/maskedinput/jquery.maskedinput-1.3.min.js');?>"></script>
@@ -14,8 +20,10 @@
     <script type="text/javascript" src="<?=asset_url('plugins/forms/timeentry/jquery.timeentry.min.js');?>"></script>
     <script type="text/javascript" src="<?=asset_url('plugins/forms/select/select2.min.js');?>"></script>
     <script type="text/javascript" src="<?=asset_url('plugins/forms/dualselect/jquery.dualListBox-1.3.min.js');?>"></script>    
+	<script type="text/javascript" src="<?=asset_url('plugins/forms/tiny_mce/tinymce.min.js');?>"></script>
 
-     <script type="text/javascript" src="<?=asset_url('plugins/misc/search/tipuesearch_set.js');?>"></script>
+	<script type="text/javascript" src="<?=asset_url('plugins/misc/prettify/prettify.js');?>"></script><!-- Code view plugin -->
+	<script type="text/javascript" src="<?=asset_url('plugins/misc/search/tipuesearch_set.js');?>"></script>
     <script type="text/javascript" src="<?=asset_url('plugins/misc/search/tipuesearch_data.js');?>"></script><!-- JSON for searched results -->
     <script type="text/javascript" src="<?=asset_url('plugins/misc/search/tipuesearch.js');?>"></script>
     
@@ -24,7 +32,7 @@
     <script type="text/javascript" src="<?=asset_url('plugins/tables/dataTables/TableTools.min.js');?>"></script>
     <script type="text/javascript" src="<?=asset_url('plugins/tables/dataTables/ZeroClipboard.js');?>"></script>
     <script type="text/javascript" src="<?=asset_url('plugins/tables/responsive-tables/responsive-tables.js');?>"></script>
-    <script type="text/javascript" src="<?=asset_url('plugins/charts/knob/jquery.knob.js');?>"></script>
+    
     <script type="text/javascript" src="<?=asset_url('plugins/misc/animated-progress-bar/jquery.progressbar.js');?>"></script>
 
     
@@ -39,15 +47,15 @@
     <!-- Init plugins -->
     <script type="text/javascript" src="<?=asset_url('js/jquery.mousewheel.js');?>"></script>
     <script type="text/javascript" src="<?=asset_url('js/jRespond.min.js');?>"></script>
-	<script type="text/javascript" src="<?=asset_url('js/main.js');?>"></script><!-- Core js functions --> 
+    <script type="text/javascript" src="<?=asset_url('js/main.js');?>"></script><!-- Core js functions -->
     <script type="text/javascript" src="<?=asset_url('js/tables.js');?>"></script>
     <script type="text/javascript" src="<?=asset_url('js/datatable.js');?>"></script>
     <script type="text/javascript" src="<?=asset_url('js/elements.js');?>"></script>
     <script type="text/javascript" src="<?=asset_url('js/jquery.poshytip.min.js');?>"></script>
     <script type="text/javascript" src="<?=asset_url('js/flexible-nav.min.js');?>"></script>
+    <script type="text/javascript" src="<?=asset_url('js/jquery-ui.min.js');?>"></script>
     <script type="text/javascript" src="<?=asset_url('js/forms.js');?>"></script>
-	
-    
+    <script type="text/javascript" src="<?=asset_url('js/form-validation.js');?>"></script>
     
 
 </html>
