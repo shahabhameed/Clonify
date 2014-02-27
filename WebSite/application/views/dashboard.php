e5b631133ca8c98b5c3221b92ccb048678b39a20
Mohsinkhan88 <mohsin88@live.com>
2014-02-27 19:26:54 +0500

    Modified File Manager GUI

--- WebSite/application/views/dashboard.php
+++ WebSite/application/views/dashboard.php
@@ -1,79 +1,105 @@
 <div id="wrapper">
-<?php 
-  include VIEW_PARTIAL_FOLDER_PATH . '/sidebar.php'; 
-?>
-        <!--Body content-->
-       <div id="content" class="clearfix">
-            <div class="contentwrapper"><!--Content wrapper-->
+    <?php
+    include VIEW_PARTIAL_FOLDER_PATH . '/sidebar.php';
+    ?>
+    <!--Body content-->
+    <div id="content" class="clearfix">
+        <div class="contentwrapper"><!--Content wrapper-->
 
-                <div class="heading">
+            <div class="heading">
 
-                    <h3>File Manager</h3>                                                                               
-                </div><!-- End .heading-->
-				
-				
-             <div class="row-fluid">
+                <h3>
+                    
 
-                        <div class="span12">
+                </h3>    
+                <ul class="breadcrumb">
+                    <li>You are here:</li>
+                    <li>
+                    </li>
+                    <li class="active">File Manager</li>
+                </ul>
+            </div><!-- End .heading-->
 
-                            <div class="box gradient">
-                                <div class="title">
-                                    <h4>
-                                        <span class="icon16 icomoon-icon-folder"></span>
-                                        <span>File manager and upload</span>
-                                    </h4>
+
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="panel panel-default gradient">
+                        <div class="panel-heading">
+                            <h4><span class="icon16 fa fa-cloud-upload"></span>
+                    <span >File Manager</span></h4>
+
+                        </div>
+                        <div class="panel-body noPad clearfix">
+
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    
+                                    <button class=" btn btn-success  pull-right col-lg-1" type="submit">Finish</button>
+                                    
                                 </div>
-                                <div class="content noPad">
-                                    <div id="elfinder"></div>
-                                </div>
+                            </div>
 
-                            </div><!-- End .box -->
 
-                        </div><!-- End .span12 -->
 
-                   </div><!-- End .row-fluid -->
-				<div>
-					<form method="post" accept-charset="utf-8" action="<?php echo base_url();?>index.php/ex_cont/saveFilesToDb" >
-					
-					<button class="btn btn-success" type="submit">Finish</button>
-				</div>
-            </div><!-- End contentwrapper -->
-        </div><!-- End #content -->
-    
-    </div><!-- End #wrapper -->
+                            <div class="row-fluid">
 
-    <script type="text/javascript" charset="utf-8">
+                                <div class="box gradient col-lg-12 ">
+
+                                    <div class="content noPad ">
+                                        <div id="elfinder"></div>
+                                    </div>
+
+                                </div><!-- End .box -->
+
+                            </div><!-- End .row-fluid -->
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div>
+                <form method="post" accept-charset="utf-8" action="<?php echo base_url(); ?>index.php/ex_cont/saveFilesToDb" >
+
+
+            </div>
+        </div><!-- End contentwrapper -->
+    </div><!-- End #content -->
+
+</div><!-- End #wrapper -->
+
+<script type="text/javascript" charset="utf-8">
     $().ready(function() {
         var elf = $('#elfinder').elfinder({
             // lang: 'ru',             // language (OPTIONAL)
-            url : '<?php echo site_url("ex_cont/elfinder_init");?>',  // connector URL (REQUIRED)
+            url: '<?php echo site_url("ex_cont/elfinder_init"); ?>', // connector URL (REQUIRED)
             uiOptions: {
-                    toolbar : [
-                        // toolbar configuration
-                        ['open'],
-                        ['back', 'forward'],
-                        ['reload'],
-                        ['home', 'up'],
-                        ['mkdir', 'mkfile', 'upload'],
-                        ['info'],
-                        ['rm'],
-                        ['search'],
-                        ['view']
-                    ]
-                },
-			contextmenu : {
-				// navbarfolder menu
-			navbar : ['open', '|', 'rm', '|', 'info'],
-
-				// current directory menu
-			cwd    : ['reload', 'back', '|', 'upload', 'mkdir', '|', 'info'],
-
-			// current directory file menu
-			files  : [
-					'getfile', '|','open', 'quicklook', '|', 'download', '|',
-					'rm', '|',  'info'
-				]
-			},	
-	}).elfinder('instance');            
+                toolbar: [
+                    // toolbar configuration
+                    ['open'],
+                    ['back', 'forward'],
+                    ['reload'],
+                    ['home', 'up'],
+                    ['mkdir', 'mkfile', 'upload'],
+                    ['info'],
+                    ['rm'],
+                    ['search'],
+                    ['view']
+                ]
+            },
+            contextmenu: {
+                // navbarfolder menu
+                navbar: ['open', '|', 'rm', '|', 'info'],
+                // current directory menu
+                cwd: ['reload', 'back', '|', 'upload', 'mkdir', '|', 'info'],
+                // current directory file menu
+                files: [
+                    'getfile', '|', 'open', 'quicklook', '|', 'download', '|',
+                    'rm', '|', 'info'
+                ]
+            },
+        }).elfinder('instance');
     });
-    </script>
+</script>
