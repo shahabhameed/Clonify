3359039b6d0a53e4a7ffaacb1c42a4837f433579
HafeezurRehman <zurmalik@yahoo.com>
2014-03-11 18:44:06 +0500

    Tow Previous Commits require this schema

--- WebSite/update.sql
+++ WebSite/update.sql
@@ -149,65 +149,26 @@ CREATE TABLE IF NOT EXISTS `clones_rnr` (
   `value` varchar(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- 06 Mar 2014
+-- ADDED ON 2014-03-06 0, BY KHIZER
 ALTER TABLE  `invocation_parameters` ADD  `min_similarity_MCC_tokens` INT NULL ,
 ADD  `min_similarity_MCC_percentage` INT NULL ;
 
--- 11 Mar 2014
--- MCC Tables Added
--- phpMyAdmin SQL Dump
--- version 4.0.9
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Mar 11, 2014 at 01:11 PM
--- Server version: 5.5.34
--- PHP Version: 5.4.22
+-- ADDED ON 2014-03-11 0, BY HAFEEZ
+ALTER TABLE scs_crossfile DROP PRIMARY KEY;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+ALTER TABLE mcc_id DROP PRIMARY KEY;
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `dssd`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `mcc_instance`
---
-
-CREATE TABLE IF NOT EXISTS `mcc_instance` (
-  `mcc_instance_id` int(11) NOT NULL,
-  `mcc_id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `method_file` (
   `mid` int(11) NOT NULL,
-  `tc` double NOT NULL,
-  `pc` double NOT NULL,
   `fid` int(11) NOT NULL,
-  `did` int(11) NOT NULL,
-  `gid` int(11) NOT NULL
+  `startline` int(11) NOT NULL,
+  `endline` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE IF NOT EXISTS `mcc_scc` (
-  `mcc_id` int(11) NOT NULL,
-  `scc_id` int(11) NOT NULL
+CREATE TABLE IF NOT EXISTS `method` (
+  `mid` int(11) NOT NULL,
+  `mname` varchar(100) NOT NULL,
+  `tokens` int(11) NOT NULL,
+  `startline` int(11) NOT NULL,
+  `endline` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE IF NOT EXISTS `mcc` (
-  `mcc_id` int(11) NOT NULL,
-  `atc` double NOT NULL,
-  `apc` double NOT NULL,
-  PRIMARY KEY (`mcc_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-ALTER TABLE  `mcc` ADD  `invocation_id` INT( 11 ) NOT NULL ;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
