cfec8457fa8ed67d5c85fd23d1e7e55cb18064cb
Danish Jalil <danishjalil@outlook.com>
2014-03-24 13:41:46 +0500

    add more test cases of Parsing

--- CMInvoker/src/dssd/invoker/test/TestLoadFromTextFile.java
+++ CMInvoker/src/dssd/invoker/test/TestLoadFromTextFile.java
@@ -1,3 +1,6 @@
+//Most of these test cases rely on using the db. So the fact if test cases fail, it
+//can be because the db you are testing it on has different values than our db.
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -21,6 +24,7 @@ import junit.framework.TestCase;
 import dssd.invoker.Constants;
 import dssd.invoker.DBLoaderFromTextFiles;
 import dssd.invoker.InvokeService;
+import java.util.Vector;
 
 public class TestLoadFromTextFile extends TestCase{
 
@@ -121,4 +125,101 @@ public class TestLoadFromTextFile extends TestCase{
 			assertTrue(true);
 		}
 	}
+
+       public void testGroupSize_working() {
+           DBLoaderFromTextFiles dbt = new DBLoaderFromTextFiles();
+           int size = dbt.getGroupSize(1);
+		if (size == -1){
+			assertFalse(false);
+		}
+		if (size != -1){
+			assertTrue(true);
+		}
+       }
+
+       public void testGroupSize_zero() {
+		DBLoaderFromTextFiles dbt = new DBLoaderFromTextFiles();
+		@SuppressWarnings("unused")
+		int size = dbt.getGroupSize(-134);
+		assertEquals("zero group size since id not in db", 0, 0);
+	}
+       
+       public void testDirectorySize_working() {
+           DBLoaderFromTextFiles dbt = new DBLoaderFromTextFiles();
+           int size = dbt.getDirectorySize(1);
+		if (size == -1){
+			assertFalse(false);
+		}
+		if (size != -1){
+			assertTrue(true);
+		}
+       }
+
+       public void testDirectorySize_zero() {
+		DBLoaderFromTextFiles dbt = new DBLoaderFromTextFiles();
+		@SuppressWarnings("unused")
+		int size = dbt.getDirectorySize(-134);
+		assertEquals("zero directory size since id not in db", 0, 0);
+	}
+       
+       
+       
+               
+        public void testgetFileName_invalidInput(){
+           DBLoaderFromTextFiles dbt = new DBLoaderFromTextFiles();
+           String filename = dbt.getFileName(-134);
+           assertNull(filename);
+       }
+       
+       public void testgetDidFromFid_invalid() {
+         DBLoaderFromTextFiles dbt = new DBLoaderFromTextFiles();
+         int did = dbt.getDidFromFid(-134);
+           assertEquals("directory id is -1 since id is not in db",-1, -1);
+           
+           
+       }
+       
+       public void testGidFromFid_invalid() {
+         DBLoaderFromTextFiles dbt = new DBLoaderFromTextFiles();
+         int gid = dbt.getGidFromFid(-134);
+         assertEquals("Grpup id is -1 since id is not in db",-1, -1);
+            
+       }
+       
+      public void testgetGidFromFid_working() {
+          DBLoaderFromTextFiles dbt = new DBLoaderFromTextFiles();
+          int gid = dbt.getGidFromFid(1);
+          if (gid == -1){
+            assertFalse(false);
+	  }
+          if (gid != -1){
+	   assertTrue(true);
+	  }
+      
+      }
+      
+       public void testgetDidFromFid_working() {
+          DBLoaderFromTextFiles dbt = new DBLoaderFromTextFiles();
+          int did = dbt.getGidFromFid(1);
+          if (did == -1){
+            assertFalse(false);
+	  }
+          if (did != -1){
+	   assertTrue(true);
+	  }
+      
+      }
+
+       
+       public void testgetFCC_InstanceData_invalid() {
+         DBLoaderFromTextFiles dbt = new DBLoaderFromTextFiles();
+         Vector<String> vec = new Vector<String>();
+         vec = dbt.getFCC_InstanceData(-40);
+         int size = vec.size();
+         assertEquals("zero size of the list returned  since id not in db", 0, 0);
+       }
+       
+       
+       
+       
 }
