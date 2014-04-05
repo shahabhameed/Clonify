cd9cd7194edee8ae60a9760f18167a9f6fabd703
shahramjaved75 <shahramjaved75@gmail.com>
2014-04-05 23:01:04 +0500

    .

---
+++ WebSite/application/views/clone_table/fcs_within_directory.php
@@ -0,0 +1,419 @@
+<style type="text/css">
+    .panel .dataTables_length {
+        margin-top: 8px;
+    }
+    div.selector {
+
+        width: 50% !important;
+
+    }
+</style>
+<script type="text/javascript" src="<?= asset_url('js/myTreeMap.js') ?>"></script>
+<div id="wrapper">
+
+    <?php
+    include VIEW_PARTIAL_FOLDER_PATH . '/sidebar.php';
+    ?>
+    <div id="content" class="clearfix">
+        <div class="contentwrapper">
+            <div class="heading">
+                <h3>File Clone Structure</h3> 
+                <ul class="breadcrumb">
+                    <li>You are here:</li>
+                    <li>
+                        <a href="<?php echo base_url(); ?>" class="tip" title="back to dashboard">
+                            <span class="icon16 fa fa-desktop"></span>
+                        </a> 
+                        <span class="divider">
+                            <span class="icon16 fa fa-caret-right"></span>
+                        </span>
+                    </li>
+                    <li class="active">File Clone Structure</li>
+                </ul>                   
+            </div>
+
+            <!-- Modal 1-->
+            <div class="modal fade" id="qtable1" tabindex="-1" role="dialog" aria-labelledby="Table 1 Query" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                            <h4 class="modal-title" id="myModalLabel">Select * From FCS Within Directory Where</h4>
+                        </div>
+                        <div class="modal-body">
+
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <u><h4>FCS ID</h4></u>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-4" id="fcsidfilter">
+                                </div>
+                            </div>
+                            <br>
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <u><h4>No. Of Instances</h4></u>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-4" id="noofclones">
+                                </div>
+                            </div>
+                            <br>
+                            <div class="row">
+                                <div class="col-md-10">
+                                    <small>For multiple values use [], e.g for number 1 and 2 write [1,2]</small>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Modal2 -->
+            <div class="modal fade" id="qtable2" tabindex="-1" role="dialog" aria-labelledby="Table 2 Query" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                            <h4 class="modal-title" id="myModalLabel">Search * From SCC Clone Instance List Where</h4>
+                        </div>
+                        <div class="modal-body">
+                            <div class="row">
+                                <div class="col-md-3">
+                                    <u><h4>Clone Id</h4></u>
+                                </div>
+                                <div class="col-md-4" id="cloneId">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row-fluid">
+                <div class="col-md-9">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="panel panel-default gradient">
+                                <div class="panel-heading min">
+                                    <h4><span> <i class="fa fa-list-alt fa-2"></i> FCS Within Directory Table</span></h4>
+                                    <span class="loader" style="top:15px;cursor:pointer;">
+                                        <i class="fa fa-search fa-4" data-toggle="modal" data-target="#qtable1"></i>
+                                    </span>
+                                    <a href="#"  id="pannel1" class="minimize" style="display: inline;">Minimize</a>
+
+                                </div>
+                                <div class="panel-body noPad clearfix">
+                                    <table cellpadding="0" cellspacing="0" border="0" class="responsive dynamicTablefcs display table table-bordered" width="100%">
+                                        <thead>
+                                            <tr>
+                                                <th>No</th>
+                                                <th>FCS ID</th>
+                                                <th>Structure (FCC ID, ...)</th>                        
+                                                <th>Directory Id</th>
+                                                <th>No. Of Instances</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php
+                                            $counter = 0;
+                                            if ($parent_table_data)
+                                                foreach ($parent_table_data as $data) {
+                                                    $counter++;
+                                                    ?>
+                                                    <tr class="list_view" data-sccid="<?php echo $data['fcs_indir_id']; ?>">
+                                                        <td><?php echo $counter; ?></td>
+                                                        <td><?php echo $data['fcs_indir_id']; ?></td>                          
+                                                        <td ><?php echo isset($data['fcc_ids']) ? $data['fcc_ids'] : "-"; ?></td>
+                                                        <td><?php echo isset($data['directory_id']) ? $data['directory_id'] : '-'; ?></td>
+                                                        <td><?php echo isset($data['members']) ? $data['members'] : '-'; ?></td>
+
+                                                    </tr>
+                                                <?php } ?>                        
+                                        </tbody>
+                                        <tfoot>
+                                            <tr>
+                                                <th>No</th>
+                                                <th>FCS ID</th>
+                                                <th>Structure (FCC ID, ...)</th>                        
+                                                <th>Directory Id</th>
+                                                <th>No. Of Instances</th>
+                                            </tr>
+                                        </tfoot>                     
+                                    </table>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <?php
+                    if ($secondary_table_rows)
+                        $secondary_table_rows = $secondary_table_rows ? $secondary_table_rows : array();
+                    foreach ($secondary_table_rows as $fcs_id => $data) {
+                        ?>
+                        <div class="row scc_instance_list" id="fcs_instance_list_<?php echo $fcs_id; ?>">
+                            <div class="col-md-12">
+                                <div class="panel panel-default gradient">
+                                    <div class="panel-heading min">
+                                        <h4><span> <i class="fa fa-list-alt fa-2"></i>FCS Secondary Table</span></h4>
+                                        <span class="loader" style="top:15px;cursor:pointer;">
+                                            <i class="fa fa-search fa-4" data-toggle="modal" data-target="#qtable2"></i>
+                                        </span>
+                                        <a href="#"  id="pannel2" class="minimize" style="display: inline;">Minimize</a>
+                                    </div>
+
+                                    <div class="panel-body noPad clearfix">
+                                        <table cellpadding="0" cellspacing="0" border="0" class="responsive dataTable display table table-bordered" width="100%">
+                                            <thead>
+                                                <tr>                        
+                                                    <th>No.</th>
+                                                    <th>Clone ID</th>
+                                                    <th>Structure(FID, ...)</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <?php
+                                                $counter = 0;
+                                                $data = $data ? $data : array();
+                                                foreach ($data as $ingroup_id => $fids) {
+                                                    $counter++;
+                                                    $new_fids = join(", ", $fids);
+                                                    $temp_filds = join(",", $fids);
+                                                    ?>
+                                                    <tr class="code_view" data-files="<?php echo $temp_filds; ?>">
+                                                        <td><?php echo $counter; ?></td>
+                                                        <td><?php echo $ingroup_id; ?></td>
+                                                        <td><?php echo $new_fids; ?></td>
+                                                    </tr>
+                                                <?php } ?>
+                                            </tbody> 
+                                            <tfoot>
+                                                <tr>
+                                                    <th>No.</th>
+                                                    <th>Clone ID</th>
+                                                    <th>Structure(FID, ...)</th>
+                                                </tr>
+                                            </tfoot>                       
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    <?php } ?>
+                    <div class="row code-window-containter">
+                        <div class="col-md-12">
+                            <div class="panel panel-default gradient">
+                                <div class="panel-heading">
+                                    <h4><span>Code Window</span></h4>
+                                </div>
+                                <div class="panel-body noPad clearfix">
+                                    <div class="">
+                                        <div class="col-md-6 panel-heading">
+                                            <h4><span id="file1"></span></h4>
+                                        </div>
+                                        <div class="col-md-6 panel-heading">
+                                            <h4><span id="file2"></span></h4>
+                                        </div>
+                                    </div>
+                                    <div class="code-window1">
+                                        <div class="col-md-11 padding15 code-window responsive" id="code_window1" >                    
+                                        </div>
+
+                                        <div class="col-md-1" id="code_map1" style="padding:0px;padding-right:5px;width:65px !important;">
+                                        </div>
+                                    </div>
+
+                                    <div class="code-window2">
+                                        <div class="col-md-1" id="code_map2" style="padding:0px;width:65px !important;">
+                                        </div>
+                                        <div class="col-md-5 padding15 code-window responsive" id="code_window2">
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3" style="border-left:1px solid;max-height:600px;overflow-y:scroll;overflow-x:hidden;">
+                    <div class="todo">
+                        <h5>Navigation</h5>
+                        <ul id="treeDemo" class="ztree"></ul>
+                    </div>
+
+                </div>
+                <div class="row" id="treeMapBlock">
+                    <div class="col-lg-9">
+
+
+                        <div class="panel panel-default">
+
+                            <div class="form-group">
+
+                                <div class="col-lg-12">
+
+                                    <div class="panel panel-default">
+
+                                        <div class="panel-heading">
+                                            <h4><span class="icon16 icomoon-icon-equalizer-2"></span><span>Tree Map View</span> </h4><a href="#" class="minimize">Minimize</a>
+                                        </div>
+
+                                        <div class="panel-body">
+                                            <div class="form-group">
+
+                                                <div class="col-lg-12">
+                                                    <div id="treemap" align="center" class="marginL20 col-lg-12 "></div>	
+
+
+                                                </div>
+
+
+                                            </div>
+                                        </div><!-- End .panel body -->
+                                    </div>
+
+                                </div><!-- End .span8 -->
+
+
+                            </div>
+                        </div><!-- End .row -->
+                    </div>
+                </div>
+            </div><!-- End contentwrapper -->
+        </div><!-- End #content -->
+    </div><!-- End #wrapper -->
+
+    <script>
+
+
+      
+    $(function() {
+    var data = [
+   
+<?php
+
+    if ($treemapdata)
+        {
+
+
+    foreach ($treemapdata as $dirList => $data) {
+
+        parseDirStructure($data, "Root");
+    }
+}
+?>
+
+    ];
+            loadTreeMap(data);
+    });
+    
+    
+    
+    <?php
+            function parseDirStructure($directory, $parentName)
+            {
+                 if(!empty($directory)){
+                     
+                     if($directory ['dname']==""){
+                         $dname = $parentName;
+                     }
+                     else
+                     {
+                        $dname = $directory ['dname'];
+                     }
+                    $dsize = $directory['dsize'];        
+                    
+                    createParent($directory, $parentName);
+                   
+                    $children = $directory['children'];
+                    if(!empty($children)){
+                        foreach($children as $child => $childData)
+                         {
+                            parseDirStructure($childData, $dname);
+                         }
+                    }
+
+                     $files = $directory['files'];
+                     traverseFiles($files);
+            }
+            }
+
+
+    function createParent($directory, $parentName)
+    {
+    echo "{";
+            if ($directory['dname'] != "")
+    {
+             echo "label: '".$directory['dname']."',";
+             echo "value: 1,";
+             echo "parent: '".$parentName."',";
+ 
+    }
+    else
+    {
+            echo "label: '".$parentName."',";
+            echo "value: null,";
+    }  
+            echo "color: '#".random_color()."',";
+            echo "},";
+    }
+    function random_color_part() {
+    return str_pad(dechex(mt_rand(0, 255)), 2, '0', STR_PAD_LEFT);
+            }
+    function random_color() {
+    return random_color_part().random_color_part().random_color_part();
+            }
+    function traverseFiles($files)
+    {
+        if(!empty($files)){
+        foreach ($files as $file => $filedata) {
+
+            echo "{";
+            echo "label: '".$filedata['filename']."',";
+            echo "value: ".$filedata['fsize'].",";
+            echo "parent: '".$filedata['dname']."',";
+           /* if($filedata['fsize']>5000)
+            {
+                echo "color: '#"."73DD58"."',";
+            }
+            else
+            {
+                 echo "color: '#"."1292AA"."',";
+            }
+             */
+            
+            echo "color: '#".random_color()."',";
+            echo "data: {description: '".$filedata['dname'].$filedata['filename']."</br>File Size: ".$filedata['fsize']."', title: '".$filedata['filename']."'}";
+            echo "},";
+    }
+    }
+    }
+    
+    function showTreeMap()
+    {}
+    
+    ?>  
+
+      //var zNodes = <?php echo $treedata ?>;
+        
+        
+        $(document).ready(function() {
+            $(".list_view").on("click", function() {
+                Clonify.FCS.viewInstanceWithinDirectory($(this).data("sccid"));
+
+                showTreeMap($(this).data("fcs_indir_id"));
+
+                event.preventDefault();
+                return false;
+            });
+        });
+    </script>
