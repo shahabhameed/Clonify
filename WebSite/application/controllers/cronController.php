60520e3cfbbfa0a8f11d513b1a41c6092564d8ca
HafeezurRehman <zurmalik@yahoo.com>
2014-04-13 17:10:46 +0500

    Time Out !
    I am not going to write a cron job ! So there !
    
    Well Due to  Time Out after every 300 Seconds I am not refreshing the
    page every 5 seconds .

--- WebSite/application/controllers/cronController.php
+++ WebSite/application/controllers/cronController.php
@@ -16,33 +16,29 @@ Class CronController extends CI_Controller
     {
 		$user_name = $this->tank_auth->get_username();
 		$this->session->set_userdata(array('user_name'=>$user_name));
-
-		for(;;){
 		
-			echo "checking again <br />";
-			
-			$listOfInvocationsForEmails = $this->cronModel->getUserInvocationsToBeEmailed();
+		$this->load->view('cronView');
 
-			$type = 'invocationComplete';
+		echo "checking ... " . date(DATE_RFC2822) . "<br /><br /><br />";
+		
+		$listOfInvocationsForEmails = $this->cronModel->getUserInvocationsToBeEmailed();
+
+		$type = 'invocationComplete';
+		
+		foreach($listOfInvocationsForEmails as $result){
+			$data = json_decode(json_encode($result), true);
+			echo $data['email'] . "<br/>" . $data['status'] . "<br/>" . $data['invoked_time'] . "<br/>" . $data['invocation_name'] . "<br/><br/>";
 			
-			foreach($listOfInvocationsForEmails as $result){
-				$data = json_decode(json_encode($result), true);
-				echo $data['email'] . "<br/>" . $data['status'] . "<br/>" . $data['invoked_time'] . "<br/>" . $data['invocation_name'] . "<br/><br/>";
-				
-				$this->load->library('email');
-				$this->email->from($this->config->item('webmaster_email', 'tank_auth'), $this->config->item('website_name', 'tank_auth'));
-				$this->email->reply_to($this->config->item('webmaster_email', 'tank_auth'), $this->config->item('website_name', 'tank_auth'));
-				$this->email->to($data['email']);
-				$this->email->subject('Invocation Finished ['. $data['invocation_name'] . ']');
-				$this->email->message($this->load->view('email/'.$type.'-html', $data, TRUE));
-				$this->email->set_alt_message($this->load->view('email/'.$type.'-txt', $data, TRUE));
-				$this->email->send();
-				
-				$this->cronModel->setUserInvocationsEmailSentFlag($data['id']);
-			}
+			$this->load->library('email');
+			$this->email->from($this->config->item('webmaster_email', 'tank_auth'), $this->config->item('website_name', 'tank_auth'));
+			$this->email->reply_to($this->config->item('webmaster_email', 'tank_auth'), $this->config->item('website_name', 'tank_auth'));
+			$this->email->to($data['email']);
+			$this->email->subject('Invocation Finished ['. $data['invocation_name'] . ']');
+			$this->email->message($this->load->view('email/'.$type.'-html', $data, TRUE));
+			$this->email->set_alt_message($this->load->view('email/'.$type.'-txt', $data, TRUE));
+			$this->email->send();
 			
-			sleep(5);
+			$this->cronModel->setUserInvocationsEmailSentFlag($data['id']);
 		}
-   }
-
+	}
 }
--- WebSite/application/views/cronView.php
+++ WebSite/application/views/cronView.php
@@ -1,47 +1,12 @@
-<div id="wrapper">
-    <?php include VIEW_PARTIAL_FOLDER_PATH . '/sidebar.php'; ?>
-    <form action=" cronController/cron_job_save" method="post" name="adminForm" id="adminForm" onsubmit="return check(this)">
-        <table border="0" class="tablehead" width="100%">
-            <tr>
-                <th width="25%" align="left">Set Timings</th>
-                <th width="75%" align="left"></th>
-            </tr>
-            <tr>
-                <td align="left">Min</td>
-                <td align="left">
-                    <input class="text_area" type="text" name="min" id="min" />
-                </td>
-            </tr>
-            <tr>
-                <td align="left">Hours</td>
-                <td align="left">
-                    <input class="text_area" type="text" name="hour" id="hour" />(0-23)
-                </td>
-            </tr>
-            <tr>
-                <td align="left">Day of Month</td>
-                <td align="left">
-                    <input type="text" name="day_of_month" id="day_of_month" />(1-31)
-                </td>
-            </tr>
-            <tr>
-                <td align="left">Month</td>
-                <td align="left">
-                    <input type="text" name="month" id="month" />(1-12)
-                </td>
-            </tr>
-            <tr>
-                <td align="left">Day of Week</td>
-                <td align="left">
-                    <input type="text" name="day_of_week" id="day_of_week" />(0-6)
-                </td>
-            </tr>
-            <tr>
-                <td align="left"></td>
-                <td align="left">
-                    <input name="Save" type="submit" class="button" id="Save" value="Create" />
-                </td>
-            </tr>
-        </table>
-    </form>
-</div>
+
+<script>
+
+	function auto_reload()
+    {
+		window.location = "<?php echo base_url(); ?>cronController/";
+	}
+
+</script>
+
+ <body onload="timer = setTimeout('auto_reload()', 10000);">    
+ </body>
