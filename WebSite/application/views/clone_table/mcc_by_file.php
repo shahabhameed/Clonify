0f9c7e90037fb59883af6fd7579b5996d345b341
Abdullah Hamid <abdullahroshan17@gmail.com>
2014-04-15 15:48:27 +0500

    few changes

--- WebSite/application/views/clone_table/mcc_by_file.php
+++ WebSite/application/views/clone_table/mcc_by_file.php
@@ -37,7 +37,7 @@ width: 50% !important;
             <div class="modal-content">
               <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title" id="myModalLabel">Select * From SCC Where</h4>
+                <h4 class="modal-title" id="myModalLabel">Select * From MCC Where</h4>
               </div>
               <div class="modal-body">
                 
--- WebSite/application/views/clone_table/mcs_across_file.php
+++ WebSite/application/views/clone_table/mcs_across_file.php
@@ -26,7 +26,7 @@
             <div class="modal-content">
               <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title" id="myModalLabel">Select * From SCC Across File List Where</h4>
+                <h4 class="modal-title" id="myModalLabel">Select * From MCS Across File List Where</h4>
               </div>
               <div class="modal-body">
                
