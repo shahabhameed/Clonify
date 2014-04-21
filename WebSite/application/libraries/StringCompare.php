a40cf4799aaa769af917ae4db94f1ae5f42933af
Rohan <rohan.jalil@gmail.com>
2014-04-21 15:00:14 +0500

    changes

--- WebSite/application/libraries/StringCompare.php
+++ WebSite/application/libraries/StringCompare.php
@@ -181,7 +181,9 @@ class StringCompare{
 
   }  
   
-  function getDifferenceBetweenStrings1($s1, $s2, $s1_start_line, $s2_start_line) {
+  function getDifferenceBetweenStrings1($s1, $s2, $s1_start_line, $s2_start_line, $start_col, $end_col) {
+      $s1 = substr($s1, $start_col, strlen($s1));
+      $s2 = substr($s2, $start_col, strlen($s2));
       $i = 0;
       $s1_len = strlen($s1);
       $s2_len = strlen($s2);
